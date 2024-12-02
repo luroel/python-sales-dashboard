856b3e85547e7553927b92a04041c1d012ae879e
luroel <jl.rodrigueze@hotmail.com>
2024-12-02 15:33:34 -0600

    initializing streamlit_shadcn_ui

---
+++ dashboard.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import streamlit as st
+import streamlit_shadcn_ui as ui
+from local_components import card_container
+
+#Var
+dollar_symbol = '$'
+
+st.set_page_config(layout="wide", page_title="Panel de Facturación", page_icon=":bar_chart:")
+st.title("Panel de Facturación")
+st.markdown('##')
+
+
+
